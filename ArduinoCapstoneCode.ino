4e9f85e61235a6f5f1d0ce38e309eae00088898a
Eightfingers <james.tps09@gmail.com>
2020-05-09 20:18:54 +0800

    Initial commit

---
+++ ArduinoCapstoneCode.ino
@@ -0,0 +1,86 @@
+/*
+
+*/
+
+#include <Seeed_HM330X.h> //PMOD 2.5 Air quality lib
+#include <Wire.h>
+#include "Arduino.h"
+#include "SI114X.h" //Sunlight sensor
+#include "SCD30.h" // CO2 and sunlight sensor
+
+
+HM330X pm_Sensor;
+uint8_t buf[30];
+SI114X SI1145 = SI114X();
+
+
+const char* str[] = {"pm_Sensor num: ", "PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM2.5 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM10 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM1.0 concentration(Atmospheric environment,unit:ug/m3): ",
+                     "PM2.5 concentration(Atmospheric environment,unit:ug/m3): ",
+                     "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
+                    };
+
+/*30s*/
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  scd30.initialize();
+  Serial.println("SCD30 Raw Data");
+
+  delay(100);
+  Serial.println("Beginning HM330X");
+  if (pm_Sensor.init()) {
+    Serial.println("HM330X init failed!!!");
+    while (1);
+  }
+
+  Serial.println("Beginning Si1145!");
+  while (!SI1145.Begin()) {
+    Serial.println("Si1145 is not ready!");
+    delay(1000);
+  }
+  Serial.println("Si1145 is ready!");
+
+
+}
+
+
+void loop() {
+  if (pm_Sensor.read_sensor_value(buf, 29)) {
+    Serial.println("HM330X read result failed!!!");
+  }
+  parse_result_value(buf);
+  parse_result(buf);
+  Serial.println("");
+  delay(3000);
+
+  Serial.print("//--------------------------------------//\r\n");
+  Serial.print("Vis: "); Serial.println(SI1145.ReadVisible());
+  Serial.print("IR: "); Serial.println(SI1145.ReadIR());
+  //the real UV value must be div 100 from the reg value , datasheet for more information.
+  Serial.print("UV: ");  Serial.println((float)SI1145.ReadUV() / 100);
+  delay(3000);
+
+  float result[3] = {0};
+  if (scd30.isAvailable()) {
+    scd30.getCarbonDioxideConcentration(result);
+    Serial.print("Carbon Dioxide Concentration is: ");
+    Serial.print(result[0]);
+    Serial.println(" ppm");
+    Serial.println(" ");
+    Serial.print("Temperature = ");
+    Serial.print(result[1]);
+    Serial.println(" ℃");
+    Serial.println(" ");
+    Serial.print("Humidity = ");
+    Serial.print(result[2]);
+    Serial.println(" %");
+    Serial.println(" ");
+    Serial.println(" ");
+    Serial.println(" ");
+  }
+  delay(3000);
+
+}
